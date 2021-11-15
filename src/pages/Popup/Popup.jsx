c012ef887d1265f8268ed8695b2899836998defe
Neerja Gupta <neerjagupta@Neerjas-MacBook-Air.local>
2021-11-15 09:55:44 -0500

    git identity check

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -20,7 +20,7 @@ const Popup = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React!
+            hi there testing if github is accepting my identity
           </a>
         </header>
       </div>
